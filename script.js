765e9c5adacef7c495ddf66d989b65f4d7a33bac
danialzamaanpour-glitch <danialzamaanpour@gmail.com>
2026-09-12 11:53:19 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const BASE_URL = "https://api.apinex.bond/v1";
 const DEFAULT_MODEL = "free/gemini-3.8-flash";
-const API_KEY = "sk-apx0c33fc126db7cd59378fd068f1b9e15de8cbf0551567f3c";
+const API_KEY = "sk-bl-ne29MakzpTnL_sOrzSArzJDyFpJp35OWu9CYGRdc3JmylPeA";
 
 const $ = id => document.getElementById(id);
 const chat = $("chat"), input = $("messageInput"), form = $("messageForm");
